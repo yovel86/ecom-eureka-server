48b27bf8021f2dbed97e054c2787c408ba05fc33
Yovel <yovel.trichy@gmail.com>
2024-06-18 11:42:44 +0530

    Initial commit

---
+++ src/test/java/com/projects/ecom_eureka_server/EcomEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.ecom_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
